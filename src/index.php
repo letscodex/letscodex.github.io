6880c34a0b756111fa8cb4b5fc59a6424f9cd0a0
Kapil Motwani <51946007+motwanikapil@users.noreply.github.com>
2021-04-17 16:10:31 +0530

    Some Changes

--- src/index.php
+++ src/index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php File</title>
-</head>
-<body>
+
 <?php
     echo $_POST['email']."<br>";
     echo $_POST['usrname']."<br>";
     echo $_POST['password']."<br>";
     echo $_POST['confirm_password']."<br>";
 ?>
-</body>
-</html>
